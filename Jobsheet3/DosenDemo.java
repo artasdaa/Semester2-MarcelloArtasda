7b41f3d0723efeb399e8700277e7e7131fe679c6
artasdaa <cellom293@gmail.com>
2026-02-25 10:55:13 +0700

    Tugas2

--- Jobsheet3/DosenDemo.java
+++ Jobsheet3/DosenDemo.java
@@ -24,6 +24,80 @@ class Dosen {
     }
 }
 
+class DataDosen {
+
+    void dataSemuaDosen(Dosen[] arrayOfDosen) {
+        int no = 1;
+        for (Dosen d : arrayOfDosen) {
+            System.out.println("Data Dosen ke-" + no++);
+            d.tampilData();
+        }
+    }
+
+    void jumlahDosenPerJenisKelamin(Dosen[] arrayOfDosen) {
+        int pria = 0;
+        int wanita = 0;
+
+        for (Dosen d : arrayOfDosen) {
+            if (d.jenisKelamin) {
+                pria++;
+            } else {
+                wanita++;
+            }
+        }
+
+        System.out.println("Jumlah Dosen Pria   : " + pria);
+        System.out.println("Jumlah Dosen Wanita : " + wanita);
+    }
+
+    void rerataUsiaDosenPerJenisKelamin(Dosen[] arrayOfDosen) {
+        int totalPria = 0, totalWanita = 0;
+        int jumlahPria = 0, jumlahWanita = 0;
+
+        for (Dosen d : arrayOfDosen) {
+            if (d.jenisKelamin) {
+                totalPria += d.usia;
+                jumlahPria++;
+            } else {
+                totalWanita += d.usia;
+                jumlahWanita++;
+            }
+        }
+
+        double rataPria = jumlahPria == 0 ? 0 : (double) totalPria / jumlahPria;
+        double rataWanita = jumlahWanita == 0 ? 0 : (double) totalWanita / jumlahWanita;
+
+        System.out.println("Rata-rata Usia Dosen Pria   : " + rataPria);
+        System.out.println("Rata-rata Usia Dosen Wanita : " + rataWanita);
+    }
+
+    void infoDosenPalingTua(Dosen[] arrayOfDosen) {
+        Dosen tertua = arrayOfDosen[0];
+
+        for (Dosen d : arrayOfDosen) {
+            if (d.usia > tertua.usia) {
+                tertua = d;
+            }
+        }
+
+        System.out.println("Dosen Paling Tua:");
+        tertua.tampilData();
+    }
+
+    void infoDosenPalingMuda(Dosen[] arrayOfDosen) {
+        Dosen termuda = arrayOfDosen[0];
+
+        for (Dosen d : arrayOfDosen) {
+            if (d.usia < termuda.usia) {
+                termuda = d;
+            }
+        }
+
+        System.out.println("Dosen Paling Muda:");
+        termuda.tampilData();
+    }
+}
+
 public class DosenDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -54,12 +128,22 @@ public class DosenDemo {
             daftarDosen[i] = new Dosen(kode, nama, jenisKelamin, usia);
         }
 
-        System.out.println("\n===== DATA DOSEN =====");
-        int no = 1;
-        for (Dosen d : daftarDosen) {
-            System.out.println("Data Dosen ke-" + no++);
-            d.tampilData();
-        }
+        DataDosen data = new DataDosen();
+
+        System.out.println("\n===== SEMUA DATA DOSEN =====");
+        data.dataSemuaDosen(daftarDosen);
+
+        System.out.println("\n===== JUMLAH DOSEN PER JENIS KELAMIN =====");
+        data.jumlahDosenPerJenisKelamin(daftarDosen);
+
+        System.out.println("\n===== RATA-RATA USIA DOSEN PER JENIS KELAMIN =====");
+        data.rerataUsiaDosenPerJenisKelamin(daftarDosen);
+
+        System.out.println("\n===== DOSEN PALING TUA =====");
+        data.infoDosenPalingTua(daftarDosen);
+
+        System.out.println("\n===== DOSEN PALING MUDA =====");
+        data.infoDosenPalingMuda(daftarDosen);
 
         sc.close();
     }
