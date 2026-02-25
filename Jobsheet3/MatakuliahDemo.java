3923f91cae8207dfbd7e2c857f85d85b1e46cb4d
artasdaa <cellom293@gmail.com>
2026-02-25 10:30:01 +0700

    Percobaan3 no 2 dan 3

--- Jobsheet3/MatakuliahDemo.java
+++ Jobsheet3/MatakuliahDemo.java
@@ -6,12 +6,45 @@ import java.util.Scanner;
     public String kode;
     public String nama;
     public int sks;
-    public int jumlahJam;   
+    public int jumlahJam;
+
+    public Matakuliah() {
+    }
+
     public Matakuliah(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+
+    public void tambahData() {
+        Scanner sc = new Scanner(System.in);
+        String dummy;
+
+        System.out.print("Kode       : ");
+        kode = sc.nextLine();
+
+        System.out.print("Nama       : ");
+        nama = sc.nextLine();
+
+        System.out.print("SKS        : ");
+        dummy = sc.nextLine();
+        sks = Integer.parseInt(dummy);
+
+        System.out.print("Jumlah Jam : ");
+        dummy = sc.nextLine();
+        jumlahJam = Integer.parseInt(dummy);
+
+        System.out.println("----------------------------------");
+    }
+
+    public void cetakInfo() {
+        System.out.println("Kode       : " + kode);
+        System.out.println("Nama       : " + nama);
+        System.out.println("SKS        : " + sks);
+        System.out.println("Jumlah Jam : " + jumlahJam);
+        System.out.println("----------------------------------");
     }
 }
 
