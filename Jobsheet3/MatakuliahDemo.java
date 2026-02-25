aff3a77df9c77c3761af787170b200a33487c9a3
artasdaa <cellom293@gmail.com>
2026-02-25 10:25:08 +0700

    Percobaan3

--- Jobsheet3/MatakuliahDemo.java
+++ Jobsheet3/MatakuliahDemo.java
@@ -1,13 +1,12 @@
 package Jobsheet3;
 
 import java.util.Scanner;
-    
+
     class Matakuliah {
     public String kode;
     public String nama;
     public int sks;
-    public int jumlahJam;
-
+    public int jumlahJam;   
     public Matakuliah(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
