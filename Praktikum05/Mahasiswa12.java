72b4d0724da8b9a354b94dd538fe14d52b5c01d5
artasdaa <cellom293@gmail.com>
2026-04-01 09:31:56 +0700

    Praktikum 2

---
+++ Praktikum05/Mahasiswa12.java
@@ -0,0 +1,23 @@
+package Praktikum05;
+
+public class Mahasiswa12 {
+    String nim, nama, kelas;
+    double ipk;
+
+    Mahasiswa12() {
+    }
+
+    Mahasiswa12 (String nm, String name, String kls, double ip) {
+        this.nim = nm;
+        this.nama = name;
+        this.kelas = kls;
+        this.ipk = ip;
+    }
+
+    void tampilInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+}
---
+++ Praktikum05/MahasiswaBerprestasi12.java
@@ -0,0 +1,34 @@
+package Praktikum05;
+
+public class MahasiswaBerprestasi12 {
+    Mahasiswa12[] listMhs = new Mahasiswa12[5];
+    int idx;
+
+    void tambah(Mahasiswa12 m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh!");
+        }
+    }
+
+    void tampil() {
+        for (int i = 0; i < idx; i++) {
+            listMhs[i].tampilInformasi();
+            System.out.println("---------------------------");
+        }
+    }
+
+    void bubbleSort() {
+        for (int i = 0; i < idx - 1; i++) {
+            for (int j = 1; j < idx - i; j++) {
+                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
+                    Mahasiswa12 tmp = listMhs[j];
+                    listMhs[j] = listMhs[j - 1];
+                    listMhs[j - 1] = tmp;
+                }
+            }
+        }
+    }
+}
