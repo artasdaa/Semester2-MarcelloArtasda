80fee9b8b9adf779805f0a2f392122ed9b691430
artasdaa <cellom293@gmail.com>
2026-04-01 08:19:09 +0700

    SORTING – SELECTION SORT

--- Praktikum05/SortingMain12.java
+++ Praktikum05/SortingMain12.java
@@ -3,15 +3,16 @@ package Praktikum05;
 public class SortingMain12 {
     public static void main(String[] args) {
 
-        int a[] = {20, 10, 2, 7, 12};
+        int b[] = {30, 20, 2, 8, 14};
 
-        Sorting12 dataurut1 = new Sorting12(a, a.length);
+        Sorting12 dataurut2 = new Sorting12(b, b.length);
 
-        System.out.println("Data awal 1");
-        dataurut1.tampil();
-        dataurut1.bubbleSort();
+        System.out.println("Data awal 2");
+        dataurut2.tampil();
+        dataurut2.bubbleSort();
+        dataurut2.SelectionSort();
 
         System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
-        dataurut1.tampil();
+        dataurut2.tampil();
     }
 }
