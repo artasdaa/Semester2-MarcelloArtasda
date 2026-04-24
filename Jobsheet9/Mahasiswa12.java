09028599f413c6e7c4bc3477f5c1394c895b8bab
artasdaa <cellom293@gmail.com>
2026-04-24 21:09:23 +0700

    Praktikum1

---
+++ Jobsheet9/Mahasiswa12.java
@@ -0,0 +1,17 @@
+package Jobsheet9;
+
+public class Mahasiswa12 {
+    String nim, nama, kelas;
+    int nilai;
+
+    public Mahasiswa12(String nim, String nama, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+        this.nilai = -1; 
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+}
